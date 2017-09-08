9ee178e0269f1dead945d0763dec7f2c200996e5
Ubuntu <ubuntu@ip-172-16-2-220.ap-northeast-1.compute.internal>
2017-09-08 06:23:00 +0000

    first release

---
+++ README.txt
@@ -0,0 +1,19 @@
+# masking-logfile
+
+## 機能
+- ログファイルの特定箇所をマスキングする
+- 個人情報などのマスキングをする時に便利
+- sedコマンドで正規表現を使って置換
+
+## 使い方
+
+```
+git clone https://github.com/makaaso/masking-logfile.git
+cd masking-logfile
+./masking-logfile.sh
+```
+
+## 補足
+- 実際に利用するときはmasking-logfile.shの「sample*」を適当なディレクトリに変更
+- ファイルが圧縮されていない場合は、ファイル解凍のところをコメントアウト
+
---
+++ masking-logfile.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# ======================================================================
+# Project Name    : <Project Name>
+# File Name       : mask-logfile.sh
+# Encoding        : utf-8
+# Creation Date   : Sep. 8, 2017
+#  
+# Copyright @ 2017 Masaru Kawabata. All rights reserved.
+#  
+# This source code or any portion thereof must not be  
+# reproduced or used in any manner whatsoever.
+# ======================================================================
+
+# gunzip compress file
+## get target files
+ls -ld sample*.gz | awk '{print $9}' > gzipfile.lst
+
+## gunzip target files
+while read GZIPFILE
+do
+    gunzip $GZIPFILE
+done < ./gzipfile.lst
+
+# mask log file
+## get target files
+ls -ld sample* | awk '{print $9}' > logfile.lst
+
+## mask target file
+while read LOGFILE
+do
+    cat ${LOGFILE} |\
+    sed -e 's/\\//g' \
+        -e 's/"email"=>"[^@ ]*@[^@]*\.[^@ ]*"/"email"=>"[FILTERED]"/g' \
+        -e 's/"name"=>"[^ ]*[^]*\.[^ ]*"/"name"=>"[FILTERED]"/g' \
+        -e 's/"name"=>"[^ ]* [^]*\.[^ ]*"/"name"=>"[FILTERED]"/g' \
+        -e 's/"tel"=>"[^ ]*[^]*\.[^ ]*"/{"tel"=>"[FILTERED]"/g' \
+        -e 's/"address_detail"=>"[^ ]*[^]*\.[^ ]*"/"address_detail"=>"[FILTERED]"/g' \
+        -e 's/"account_number"=>"[^ ]*[^]*\.[^ ]*"/"account_number"=>"[FILTERED]"/g' \
+        -e 's/"account_name_kana"=>"[^ ]*[^]*\.[^ ]*"/"account_name_kana"=>"[FILTERED]"/g' \
+        -e 's/"bank_name"=>"[^ ]*[^]*\.[^ ]*"/"bank_name"=>"[FILTERED]"/g' \
+        -e 's/"bank_code"=>"[^ ]*[^]*\.[^ ]*"/"bank_code"=>"[FILTERED]"/g' \
+        -e 's/"branch_name"=>"[^ ]*[^]*\.[^ ]*"/"branch_name"=>"[FILTERED]"/g' \
+        -e 's/"branch_number"=>"[^ ]*[^]*\.[^ ]*"/"branch_number"=>"[FILTERED]"/g' \
+        -e 's/"address"=>"[^ ]*[^]*\.[^ ]*"/"address"=>"[FILTERED]"/g' \
+        -e 's/"aid"=>"[^ ]*[^]*\.[^ ]*"/"aid"=>"[FILTERED]"/g' \
+        -e 's/"user_name"=>"[^ ]*[^]*\.[^ ]*"/"user_name"=>"[FILTERED]"/g' \
+        -e 's/"primary_key"=>"[^ ]*[^]*\.[^ ]*"/"primary_key"=>"[FILTERED]"/g' \
+        -e 's/"identifier"=>"[^ ]*[^]*\.[^ ]*"/"identifier"=>"[FILTERED]"/g' \
+        -e 's/"encrypted_password"=>"[^ ]*[^]*\.[^ ]*"/"encrypted_password"=>"[FILTERED]"/g' \
+        -e 's/"password_salt"=>"[^ ]*[^]*\.[^ ]*"/"password_salt"=>"[FILTERED]"/g' \
+        -e 's/"reset_password_token"=>"[^ ]*[^]*\.[^ ]*"/"reset_password_token"=>"[FILTERED]"/g' \
+        -e 's/"contact_email"=>"[^ ]*[^]*\.[^ ]*"/"contact_email"=>"[FILTERED]"/g' \
+        -e 's/"birthday"=>"[^ ]*[^]*\.[^ ]*"/"birthday"=>"[FILTERED]"/g' \
+        -e 's/"confirmation_token"=>"[^ ]*[^]*\.[^ ]*"/"confirmation_token"=>"[FILTERED]"/g' \
+        -e 's/"unconfirmed_email"=>"[^ ]*[^]*\.[^ ]*"/"unconfirmed_email"=>"[FILTERED]"/g' > ${LOGFILE}.tmp
+    mv ${LOGFILE}.tmp ${LOGFILE}
+done < ./logfile.lst
+
